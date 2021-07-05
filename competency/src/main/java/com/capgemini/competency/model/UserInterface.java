ec79929ceee4e88843e9bc4a7c715dd388dd1308
Himanshu6115 <86473642+Himanshu6115@users.noreply.github.com>
2021-07-05 15:11:11 +0530

    Delete competency directory

--- competency/src/main/java/com/capgemini/competency/model/UserInterface.java
+++
@@ -1,9 +0,0 @@
-package com.capgemini.competency.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserInterface extends JpaRepository<User,Integer>{
-
-	User findAllByUsername(String username);
-
-}
